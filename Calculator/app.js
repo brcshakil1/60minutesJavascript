67e1f9f36f660c6cc3d0ac5983e8c16e1aa0a8bf
brcshakil1 <brcshakil72@gmail.com>
2023-03-18 12:31:10 +0600

    created a simple caluculator

---
+++ Calculator/app.js
@@ -0,0 +1,30 @@
+
+const display = document.querySelector('.result')
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        switch(e.target.innerText) {
+            case 'AC':
+                display.innerText = '';
+                break;
+            case '←':
+                display.innerText = display.slice(0, -1);
+                break;
+            case '%':
+                display.innerText = eval(display.innerText) / 100;
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Error!';
+                }
+                break;
+            default: 
+                display.innerText += e.target.innerText
+        }
+    })
+    console.log()
+})
